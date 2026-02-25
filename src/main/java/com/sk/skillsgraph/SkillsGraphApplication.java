c983dd68ecafad5564bee874396bdf7a8f32360b
yopaz-anndt <anndt@yopaz.dev>
2026-02-25 10:51:11 +0700

    ini

---
+++ src/main/java/com/sk/skillsgraph/SkillsGraphApplication.java
@@ -0,0 +1,13 @@
+package com.sk.skillsgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillsGraphApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sk/skillsgraph/SkillsGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.skillsgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsGraphApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
